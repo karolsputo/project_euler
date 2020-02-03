f107bab1e931c15cd2973165ebab038006e047b4
karolsputo <R}S(i)G"7.h5q3Yb>
2020-02-03 23:58:00 +0100

    Take a stab at the first problem using Common Lisp.

---
+++ problem1.el
@@ -0,0 +1,10 @@
+(defun sum-of-multiples(n, m)
+  "Return sum of multiples of numbers passed in m up to the number n."
+  (setq count 0)
+  (loop
+   (setq count (+ count 1))
+   (if (= (mod i o) 0)
+   (if (= count (- n 1)))
+   )
+  )
+)
